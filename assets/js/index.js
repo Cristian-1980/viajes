c1bf9e3aba2ee4ed1b6b09e8eb7c738f53446e0a
Cristian-1980 <castro.cris02@gmail.com>
2024-05-16 23:24:51 -0400

    arreglo js

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,6 @@ $(function () {
     var connectVarios = document.getElementsByClassName("connectiontooltip");
     console.log("mi boton class connectVarios -> ", connectVarios);
   
-    // * y solo map connectVarios
     [...connectVarios].map((element) => {
       return new bootstrap.Tooltip(element);
     });
@@ -10,14 +9,9 @@ $(function () {
     var miBttnConectClass = $(".connect");
     console.log("mi boton class connect -> ", miBttnConectClass);
   
-    // new bootstrap.Tooltip(miBttnConectClass[0]) // <-- activo toolTip a un solo botón
-  
-    // miBttnConectClass es un objeto indexado tipo {0:"..."", 1: "...", prevData <- y con otra info más}
-    // activo toolTip a varios botones
     for (const key in miBttnConectClass) {
       if (!isNaN(parseInt(key))) {
-        // isNaN(3) -> false  |  isNaN("3") -> false  |  isNaN("hola") -> true
-        new bootstrap.Tooltip(miBttnConectClass[key]);
+                new bootstrap.Tooltip(miBttnConectClass[key]);
       }
     }
   
